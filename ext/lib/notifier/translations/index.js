9105bd7f9dc8ce69a38b637206cda269d17e5a6e
wencha <wencha_@hotmail.com>
2020-04-13 14:54:23 -0300

    Override de footer/signature de mails

--- ext/lib/notifier/translations/index.js
+++ ext/lib/notifier/translations/index.js
@@ -1,13 +1,18 @@
-const translations = require('democracyos-notifier/lib/translations')
+const translationsNotifier = require('democracyos-notifier/lib/translations')
+const translations = require('lib/backend/translations')
 const config = require('lib/config')
 const {protocol, host} = config
 const homeUrl = `${protocol}://${host}`
 
-const t = translations.t
+const tNotifier = translationsNotifier.t
+const ts = translations
 
-const overrides = {
+let t, overrides;
+
+t = ts.es
+overrides = {
   'templates.email.greeting': 'Hola {userName},',
-  'templates.email.signature': `${config.organizationName} - ${config.bajadaPlataforma}`,
+  'templates.email.signature': `${t["common.app-name"]} - ${t["homepage.presentation.slogan"]}`,
 
   'templates.welcome-email.subject': 'Bienvenido a ' + config.organizationName,
   'templates.welcome-email.body': `Iniciaste el proceso de registro para participar en <a href="${homeUrl}">${config.organizationName}</a>.<br><br>Para finalizarlo, hacé click <a href=\"{validateUrl}\">click aquí.</a>`,
@@ -21,6 +26,12 @@ const overrides = {
   'templates.topic-published.body': 'Un nuevo tema fue publicado:',
   'templates.topic-published.body2': 'Por favor <a href=\"{url}\">cliquea aquí</a> para verlo.'
 }
+console.log(overrides['templates.email.signature'])
+Object.assign(tNotifier.es, overrides)
 
-Object.assign(t.es, overrides)
-Object.assign(t.en, overrides)
+t = ts.pt
+overrides = {
+  'templates.email.signature': `${t["common.app-name"]} - ${t["homepage.presentation.slogan"]}`,
+}
+console.log(overrides['templates.email.signature'])
+Object.assign(tNotifier.pt, overrides)
